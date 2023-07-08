3182981c7fdd067beae49a1a2e1bffcc70ab0b97
Joonwon (Eric) Lee <joonwontech@gmail.com>
2023-07-08 13:09:38 +0900

    perspective projection and eye space
    Created a perspective projection matrix and eye space matrix to view the head at different locations.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <vector>
+#include <iostream>
 #include <cmath>
+#include <limits>
 #include "tgaimage.h"
 #include "model.h"
 #include "geometry.h"
@@ -8,10 +10,18 @@ const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
 const TGAColor green = TGAColor(0, 255, 0, 255);
 
-Model *model = NULL;
-
 const int width = 800;
 const int height = 800;
+const int depth = 255;
+
+Vec3f eye(2, 2, 3);
+Vec3f center(0, 0, 0);
+
+Model *model = NULL;
+
+int *zbuffer = NULL;
+
+Vec3f light_dir(3, 0, 0);
 
 void line(Vec2i p0, Vec2i p1, TGAImage &image, TGAColor color)
 {
@@ -45,16 +55,32 @@ void line(Vec2i p0, Vec2i p1, TGAImage &image, TGAColor color)
     }
 }
 
-void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color)
+void triangle(Vec3i t0, Vec3i t1, Vec3i t2, float ity0, float ity1, float ity2, TGAImage &image, int *zbuffer)
 {
-    if (t0.y == t1.y && t0.y == t2.y)
+    if (t0.y == t1.y && t0.y == t2.y) // degenerate triangle case
+    {
         return;
+    }
+
+    // Sort the vertices and corresponding texture coordinates by ascending order of their y coordinates
+    //  now we need to swap uvs as well
     if (t0.y > t1.y)
+    {
         std::swap(t0, t1);
+    }
+
     if (t0.y > t2.y)
+    {
         std::swap(t0, t2);
+        std::swap(ity0, ity1);
+    }
+
     if (t1.y > t2.y)
+    {
         std::swap(t1, t2);
+        std::swap(ity1, ity2);
+    }
+
     int total_height = t2.y - t0.y;
     for (int i = 0; i < total_height; i++)
     {
@@ -62,15 +88,59 @@ void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color)
         int segment_height = second_half ? t2.y - t1.y : t1.y - t0.y;
         float alpha = (float)i / total_height;
         float beta = (float)(i - (second_half ? t1.y - t0.y : 0)) / segment_height; // be careful: with above conditions no division by zero here
-        Vec2i A = t0 + (t2 - t0) * alpha;
-        Vec2i B = second_half ? t1 + (t2 - t1) * beta : t0 + (t1 - t0) * beta;
+        Vec3i A = t0 + Vec3f(t2 - t0) * alpha;
+        Vec3i B = second_half ? t1 + Vec3f(t2 - t1) * beta : t0 + Vec3f(t1 - t0) * beta;
+        float ityA = ity0 + (ity2 - ity0) * alpha;
+        float ityB = second_half ? ity1 + (ity2 - ity1) * beta : ity0 + (ity1 - ity0) * beta;
         if (A.x > B.x)
+        {
             std::swap(A, B);
+            std::swap(ityA, ityB);
+        }
         for (int j = A.x; j <= B.x; j++)
         {
-            image.set(j, t0.y + i, color); // attention, due to int casts t0.y+i != A.y
+            float phi = B.x == A.x ? 1. : (float)(j - A.x) / (B.x - A.x);
+            Vec3i P = Vec3f(A) + Vec3f(B - A) * phi;
+            float ityP = ityA + (ityB - ityA) * phi;
+            int idx = P.x + P.y * width;
+            if (P.x >= width || P.y >= height || P.x < 0 || P.y < 0)
+                continue;
+            if (zbuffer[idx] < P.z)
+            {
+                zbuffer[idx] = P.z;
+                image.set(P.x, P.y, TGAColor(255, 255, 255) * ityP);
+            }
         }
     }
+}
+
+Matrix viewport(int x, int y, int w, int h)
+{
+    Matrix m = Matrix::identity(4);
+    m[0][3] = x + w / 2.f;
+    m[1][3] = y + h / 2.f;
+    m[2][3] = depth / 2.f;
+
+    m[0][0] = w / 2.f;
+    m[1][1] = h / 2.f;
+    m[2][2] = depth / 2.f;
+    return m;
+}
+
+Matrix lookat(Vec3f eye, Vec3f center, Vec3f up)
+{
+    Vec3f z = (eye - center).normalize();
+    Vec3f x = (up ^ z).normalize();
+    Vec3f y = (z ^ x).normalize();
+    Matrix res = Matrix::identity(4);
+    for (int i = 0; i < 3; i++)
+    {
+        res[0][i] = x[i];
+        res[1][i] = y[i];
+        res[2][i] = z[i];
+        res[i][3] = -center[i];
+    }
+    return res;
 }
 
 int main(int argc, char **argv)
@@ -84,36 +154,61 @@ int main(int argc, char **argv)
         model = new Model("obj/african_head.obj");
     }
 
-    TGAImage image(width, height, TGAImage::RGB);
-    Vec3f light_dir(0, 0, -1);
-    for (int i = 0; i < model->nfaces(); i++)
+    zbuffer = new int[width * height];
+    for (int i = 0; i < width * height; i++)
     {
-        std::vector<int> face = model->face(i);
-        Vec2i screen_coords[3];
-        Vec3f world_coords[3];
-        for (int j = 0; j < 3; j++)
-        {
-            Vec3f v = model->vert(face[j]);
-            screen_coords[j] = Vec2i((v.x + 1.) * width / 2., (v.y + 1.) * height / 2.);
-            world_coords[j] = v;
-        }
-
-        // normal vector
-        Vec3f n = (world_coords[2] - world_coords[0]) ^ (world_coords[1] - world_coords[0]);
-        n.normalize();
-
-        // The intensity of illumination is equal to the scalar product of the light vector and the normal to the given triangle
-        float intensity = n * light_dir;
-
-        // If the dot product is negative, it means that the light comes from behind the polygon. We can simply ignore such polygons. Concept of backface culling.
-        if (intensity > 0)
-        {
-            triangle(screen_coords[0], screen_coords[1], screen_coords[2], image, TGAColor(intensity * 255, intensity * 255, intensity * 255, 255));
-        }
+        zbuffer[i] = std::numeric_limits<int>::min();
     }
 
-    image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
-    image.write_tga_file("output.tga");
+    { // draw the model
+        // Move camera
+        Matrix ModelView = lookat(eye, center, Vec3f(0, 10, 0));
+
+        // Perspective Projection Matrix:
+        Matrix Projection = Matrix::identity(4);
+        Matrix ViewPort = viewport(width / 8, height / 8, width * 3 / 4, height * 3 / 4);
+
+        Projection[3][2] = -1.f / (eye - center).norm();
+
+        std::cerr << ModelView << std::endl;
+        std::cerr << Projection << std::endl;
+        std::cerr << ViewPort << std::endl;
+        Matrix z = (ViewPort * Projection * ModelView);
+        std::cerr << z << std::endl;
+
+        TGAImage image(width, height, TGAImage::RGB);
+        for (int i = 0; i < model->nfaces(); i++)
+        {
+            std::vector<int> face = model->face(i);
+            Vec3i screen_coords[3];
+            Vec3f world_coords[3];
+            float intensity[3];
+            for (int j = 0; j < 3; j++)
+            {
+                Vec3f v = model->vert(face[j]);
+                screen_coords[j] = Vec3f(ViewPort * Projection * ModelView * Matrix(v));
+                world_coords[j] = v;
+                intensity[j] = model->norm(i, j) * light_dir;
+            }
+            triangle(screen_coords[0], screen_coords[1], screen_coords[2], intensity[0], intensity[1], intensity[2], image, zbuffer);
+        }
+        image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
+        image.write_tga_file("output.tga");
+    }
+
+    { // dump z-buffer (debugging purposes only)
+        TGAImage zbimage(width, height, TGAImage::GRAYSCALE);
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                zbimage.set(i, j, TGAColor(zbuffer[i + j * width]));
+            }
+        }
+        zbimage.flip_vertically(); // i want to have the origin at the left bottom corner of the image
+        zbimage.write_tga_file("zbuffer.tga");
+    }
     delete model;
+    delete[] zbuffer;
     return 0;
 }
